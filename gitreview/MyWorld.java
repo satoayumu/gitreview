9f537b70f07b5bdad15b48e9a1d4449713ef68a1
Takotubo <teira32@yahoo.co.jp>
2019-10-03 10:26:06 +0900

    B,Dがキャラ追加

--- gitreview/MyWorld.java
+++ gitreview/MyWorld.java
@@ -1,19 +1,21 @@
-// WARNING: This file is auto-generated and any changes to it will be overwritten
-import lang.stride.*;
-import java.util.*;
-import greenfoot.*;
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
+ * Write a description of class MyWorld here.
  * 
+ * @author (your name) 
+ * @version (a version number or a date)
  */
 public class MyWorld extends World
 {
 
     /**
      * Constructor for objects of class MyWorld.
+     * 
      */
     public MyWorld()
-    {
-        super(600, 400, 1);
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
     }
 }
